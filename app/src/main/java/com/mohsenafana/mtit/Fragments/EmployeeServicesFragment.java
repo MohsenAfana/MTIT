f380e2ac33ea888b74c59a200442929df52eae24
MohsenAfana <mohseneyas@gmail.com>
2019-10-14 16:39:14 +0300

    Time

--- app/src/main/java/com/mohsenafana/mtit/Fragments/EmployeeServicesFragment.java
+++ app/src/main/java/com/mohsenafana/mtit/Fragments/EmployeeServicesFragment.java
@@ -1,8 +1,12 @@
 package com.mohsenafana.mtit.Fragments;
+
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
@@ -10,9 +14,18 @@ import androidx.fragment.app.Fragment;
 import com.mohsenafana.mtit.R;
 
 public class EmployeeServicesFragment extends Fragment {
+    ImageView EmployeeVacationsIV;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+      /*  EmployeeVacationsIV=container.findViewById(R.id.EmployeeVacationsIV);
+        EmployeeVacationsIV.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getContext(), EmployeeVacationFragment.class);
+                startActivity(intent);
+            }
+        });*/
         return inflater.inflate(R.layout.fragment_employee_services,container,false);
     }
 }
